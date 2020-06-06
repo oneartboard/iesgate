e997ccd4fcce0b3a2cd48d812de84d9ba99b3c4c
Sandeep <sandeep@edunuts.com>
2020-06-07 00:30:35 +0530

    contact page

---
+++ contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/splitting.css">
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	
+	<title>IES GATE ACADEMY</title>
+</head>
+<?php include 'inc/header.php' ?>
+	<div class="table-wrapper c-container">
+		<h2 class="title  text-center text-theme spacer">
+			Contact Us
+		</h2>
+		<p>coming soon.</p>
+	</div>
+	<?php include 'inc/footer.php' ?>
+</body>
+</html>
