90c0d906707aee34046ceaa76f1559f9d2bfa0c7
Sandeep <sandeep@edunuts.com>
2020-06-07 01:20:21 +0530

    contact done

--- contact.php
+++ contact.php
@@ -17,8 +17,73 @@
 		<h2 class="title  text-center text-theme spacer">
 			Contact Us
 		</h2>
-		<p>coming soon.</p>
-	</div>
+		<ul class="tabs clearfix select-city" data-tabgroup="first-tab-group3">
+		  <li><a href="#chn" class="active">Chennai</a></li>
+		  <li><a href="#chn2">Coimbatore</a></li>
+	
+		</ul>
+		<section id="first-tab-group3" class="tabgroup">
+		  <div id="chn" class="c-tabs">
+			<div class="">
+				<p>
+			<strong>Head Office</strong></p>
+		<p>
+			New No. 50, Old No. 18, Mahalakshmi Street, Beside City Union Bank, Near Siva Vishnu Temple, T.Nagar, Chennai &ndash; 600017, Tamilnadu.</p>
+		<p>
+			Contact Numbers : +91 - 9445017000</p>
+		<p>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +91 &ndash; 9445027000</p>
+		<p>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +91 -&nbsp; 9445037000</p>
+		<p>
+			Contact Email :&nbsp;&nbsp;&nbsp;&nbsp; <a href="mailto:iesgateacademy@gmail.com">iesgateacademy@gmail.com</a></p>
+		<p>
+			<a href="enquire.php">Enquire Now</a></p>
+		<p>
+			{Map API needed}</p>
+		<p align="center">
+			<strong>Class Venues</strong></p>
+		<p align="center">
+			Class venues mentioned below are only to attend the classes.</p>
+		<p align="center">
+			Enquiries / Admissions happen at T.Nagar head office only.</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			<strong>Block &ndash; A</strong></p>
+		<p>
+			No. 34/60, 1<sup>st</sup> Main Road, CIT Nagar bus stop, Beside SBI, T.Nagar &ndash; 600035</p>
+		<p>
+			<strong>Block &ndash; B</strong></p>
+		<p>
+			Guntur Subbiah Pillai T.Nagar Girls Higher Secondary School, Bhagavantham Street, Near T.T.D temple, T.Nagar, Chennai &ndash; 600017</p>
+		<p>
+			(Note : Regular batch classes will be conducted in block -A only whereas weekend batch classes will be conducted in both block &ndash; A &amp; B as per the requirement. Both the venues are just five minute&rsquo;s walk distance from t.nagar bus terminus &amp; railway station.)</p>
+			</div>
+		  </div>
+	
+		  
+		  <div id="chn2" class="c-tabs">
+			<div class="">
+				<p>
+			<strong>Office &amp; Class venue</strong></p>
+		<p>
+			D.No. 401-E, 2<sup>nd</sup> Floor, Bhoopal Complex, Cross cut road, Gandhipuram, Coimbatore &ndash; 641012,&nbsp; Tamilnadu.</p>
+		<p>
+			Contact Numbers : +91 - 9003637000</p>
+		<p>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +91 &ndash; 9003737000</p>
+		<p>
+			Contact Email :&nbsp;&nbsp;&nbsp;&nbsp; <a href="mailto:iesgateacademycovai@gmail.com">iesgateacademycovai@gmail.com</a></p>
+		<p>
+			<a href="enquire.php">Enquire Now</a></p>
+		<p style="margin-left:1.5in;">
+			{Map API not found}</p>
+			</div>
+		  </div>
+		 
+		</section>
+		</div>
 	<?php include 'inc/footer.php' ?>
 </body>
 </html>
