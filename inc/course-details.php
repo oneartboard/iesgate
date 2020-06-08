79467f931a9d9702a85960e25e6a6f87e39008c9
Md Aqil <aqilali381@gmail.com>
2020-06-08 19:32:54 +0530

    added

---
+++ inc/course-details.php
@@ -0,0 +1,324 @@
+
+<div class="table-wrapper c-container">
+
+
+	<ul class="tabs clearfix select-city" data-tabgroup="first-tab-group3">
+		<span class="s-label">SELECTED CITY  -</span>
+	  <li><a href="#chn" class="active">Chennai</a></li>
+	  <li><a href="#chn2">Coimbatore</a></li>
+
+	</ul>
+	<section id="first-tab-group3" class="tabgroup">
+	  <div id="chn" class="c-tabs">
+		<div class="">
+		
+			<center>
+				<br>
+				<h2 class="title text-theme">
+					Join Once, Learn Till You Get Placed
+				</h2>
+		
+				<h3>
+					Pay once, Attend the classes till you get your expected rank. <br>
+		No fee for Rank Improvement Program.
+				</h3>
+				<p class="lead" style="font-size: 18px;">
+					2nd / 3rd Year Students are advised to join early to manage time in final year.
+		No additional Fee will be charged for 2 years Classroom Course.
+		
+				</p>
+				<h2 class="text-theme semibold">
+					Exclusive Weekend &  Batches 
+				</h2>
+			</center>
+			<ul class="c-accr">
+				<li class="accr-li">
+					<h4  class="accr-heading">
+						Weekend Batch Details
+					</h4>
+
+					<div class="accr-item">
+						<h4>
+						Class Venue: T. Nagar
+						</h4>
+					
+							<h4>
+								Streams Offered
+							</h4>
+							<ul class="simple-list">
+							<li>
+							- Electronics & Communication Engineering (EC)
+							</li>
+							<li>
+								- Electrical & Electronics Engineering (EC)
+
+							</li>
+								<li>
+									- Instrumentation Engineering (IN)
+
+								</li>
+								<li>
+									- Civil Engineering (CE)
+
+								</li>
+								<li>
+							- Mechanical Engineering (ME)
+
+								</li>
+								<li>
+									- Computer Science Engineering (CS)
+
+								</li>
+								<li>
+							- Computer Science Engineering (CS)
+
+								</li>
+								<li>
+									- Information Technology (IT)
+
+								</li>
+								<li>
+									- Chemical Engineering (CH)
+
+								</li>
+								<li>
+									- Petroleum Engineering (PE)
+
+								</li>
+								<li>
+									- Bio-Technology (BT)
+
+								</li>
+								<li>
+									- Aerospace Engineering (AE)
+
+								</li>
+
+							</ul>
+					
+						<h3>Class Timings</h3>
+						<p>Saturday (5pm to 8pm) Sunday, Festival & Public Holidays (8AM to 7PM)</p>
+						<table style="width: 100%" class="accr-table">
+							  <tr>
+								<th>Exam</th>
+								<th>Duration</th> 
+								<th>Fee</th>
+							  </tr>
+							  <tr>
+									<td>GATE+PSUs-2021</td>
+									<td>Upto January 2021</td>
+									<td>Rs. 25,000/-</td>
+							  </tr>
+							  <tr>
+									<td>IES+GATE+PSUs-2021</td>
+									<td>Upto April 2021</td>
+									<td>Rs. 40,000/-</td>
+							  </tr>
+							  <tr>
+									<td>GATE+PSUs-2022</td>
+									<td>Upto January 2022</td>
+									<td>Rs. 25,000/-</td>
+							  </tr>
+							  <tr>
+									<td>IES+GATE+PSUs-2022</td>
+									<td>Upto April 2022</td>
+									<td>Rs. 40,000/-</td>
+							  </tr>
+							  
+					</table>
+		
+					<p> Note: Course fee includes registration fee, GST, Study materials, Test Series (Offline & Online)</p>
+					<div class="inline-item">
+					<a class="btn btn-theme" href="enquire.php">ENQUIRE NOW</a>
+					<a class="btn btn-theme" href="enroll-now.php">ENROLL NOW</a>
+				</div>
+					</div>
+				</li>
+
+				<li class="accr-li">
+					<h4  class="accr-heading">
+						Regular Batch Details
+					</h4>
+
+				
+					<div class="accr-item">
+						<h4>
+						Class Venue: T. Nagar
+						</h4>
+						<h4>
+							Streams Offered
+						</h4>
+						<ul class="simple-list">
+							<li>
+								- Electronics & Communication Engineering (EC)
+
+							</li>
+							<li>
+							- Electrical & Electronics Engineering (EC)
+								
+							</li>
+							<li>
+							- Instrumentation Engineering (IN)
+								
+							</li>
+							<li>
+							- Civil Engineering (CE)
+								
+							</li>
+							<li>
+							- Mechanical Engineering (ME)
+								
+							</li>
+						</ul>
+						
+						
+						</p>
+						<h3>Class Timings</h3>
+						<p>5-6 days in a week and 6 to 8 hours per day.</p>
+						<table style="width: 100%" class="accr-table">
+							  <tr>
+								<th>Exam</th>
+								<th>Duration</th> 
+								<th>Fee</th>
+							  </tr>
+							  <tr>
+									<td>GATE+PSUs-2021</td>
+									<td>Upto December 2021</td>
+									<td>Rs. 40,000/-</td>
+							  </tr>
+							  <tr>
+									<td>IES+GATE+PSUs-2021</td>
+									<td>Upto April 2021</td>
+									<td>Rs. 50,000/-</td>
+							  </tr>
+							  
+							  
+					</table>
+		
+						<p> Note: Course fee includes registration fee, GST, Study materials, Test Series (Offline & Online)</p>
+		
+						<div class="inline-item">
+					<a class="btn btn-theme" href="enquire.php">ENQUIRE NOW</a>
+					<a class="btn btn-theme" href="enroll-now.php">ENROLL NOW</a>
+				</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	  </div>
+
+	  
+	  <div id="chn2" class="c-tabs">
+		<div class="">
+		
+			<center>
+				<br>
+				
+				<p class="lead" style="font-size: 18px;">
+					2nd / 3rd Year Students are advised to join early to manage time in final year.
+		No additional Fee will be charged for 2 years Classroom Course.
+		
+				</p>
+				<h2 class="text-theme semibold">
+					Exclusive Weekend & Regular Batches
+				</h2>
+			</center>
+			<ul class="c-accr">
+				<li class="accr-li">
+					<h4  class="accr-heading">
+						Weekend Batch Details
+					</h4>
+
+					<div class="accr-item">
+						<h4>
+						Class Venue - Gandhipuram
+
+						</h4>
+					
+							<h4>
+								Streams Offered
+							</h4>
+							<ul class="simple-list">
+							<li>
+							Electronics & Communication Engineering (EC)
+
+
+							</li>
+							<li>
+							Electrical & Electronics Engineering (EE)
+
+							</li>
+								<li>
+								Instrumentation Engineering (IN)
+
+								</li>
+								<li>
+								Civil Engineering (CE)
+
+								</li>
+								<li>
+								Mechanical Engineering (ME)
+
+								</li>
+							
+
+							</ul>
+					
+						<h3>Class Timings</h3>
+						<p>
+						Saturday (5PM to 8PM), Sunday, Festival & Public Holidays (8AM to 7PM)
+
+						</p>
+						<table style="width: 100%" class="accr-table">
+							  <tr>
+								<th>Exam</th>
+								<th>Duration</th> 
+								<th>Fee</th>
+							  </tr>
+							  <tr>
+							       
+
+									<td> GATE + PSUs - 2021</td>
+									<td>Upto January 2021 </td>
+									<td> Rs. 25,000/-</td>
+							  </tr>
+							  <tr>
+							    
+									<td> IES + GATE + PSUs - 2021</td>
+									<td>Upto April 2021</td>
+									<td> Rs. 40,000/-</td>
+							  </tr>
+							  <tr>
+							                                                        
+
+									<td>GATE + PSUs - 2022</td>
+									<td> Upto December 2021 </td>
+									<td> Rs. 25,000/-</td>
+							  </tr>
+							  <tr>
+							    
+									<td>IES + GATE + PSUs - 2022  </td>
+									<td> Upto April 2022</td>
+									<td> Rs. 40,000/-</td>
+							  </tr>
+							  
+					</table>
+		
+					<p> 
+					Note : Course Fee includes Registration Fee, GST, Study Materials, Test Series (Offline & Online)
+
+				</p>
+				<div class="inline-item">
+					<a class="btn btn-theme" href="enquire.php">ENQUIRE NOW</a>
+					<a class="btn btn-theme" href="enroll-now.php">ENROLL NOW</a>
+				</div>
+			
+					</div>
+				</li>
+
+			</ul>
+		</div>
+	  </div>
+	 
+	</section>
+</div>
---
+++ inc/test-structure.php
@@ -0,0 +1,380 @@
+<div class="spacer">
+			<h2 class="blue-heading text-center">
+				Tests Structure
+			</h2>
+			<div class="test-table">
+				<div class="table-wrapper">
+				
+				  <ul class="tabs clearfix" data-tabgroup="first-tab-group">
+					<li><a href="#tab1" class="active">ME</a></li>
+					<li><a href="#tab2">CE</a></li>
+					<li><a href="#tab3">EE</a></li>
+					<li><a href="#tab4">EC</a></li>
+					<li><a href="#tab5">IN</a></li>
+					<li><a href="#tab6">CS</a></li>
+					<li><a href="#tab7">CH</a></li>
+					<li><a href="#tab8">BT</a></li>
+					<li><a href="#tab9">AE</a></li>
+
+				  </ul>
+				  <section id="first-tab-group" class="tabgroup">
+					<div id="tab1" class="c-tabs">
+						<div class="flex-row">
+							<div class="flex-col col-6">
+								<h2 class="blue-heading text-center">
+									GATE ME 2021 Test series<br> 170+ Total Tests
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										68 Topic wise tests
+									</li>
+									<li>10 Multi topic wise tests
+									</li>
+									<li>
+										26 Subject wise tests
+
+									</li>
+									<li>
+										6 Multi subject wise tests
+									</li>
+									<li>12 Full length tests</li>
+									<li>21 Previous year full paper tests</li>
+									<li>28 Previous year subject wise tests</li>
+
+								</ul>
+							</div>
+							<div class="flex-col col-6 rtbm" style="border-left: 1px solid var(--theme-color);
+						">
+								<h2 class="blue-heading text-center">
+									IES ME 2021 Test Series<br> 60+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										40 Topic wise tests
+									</li>
+									
+									<li>
+										10 Subject wise tests
+
+									</li>
+									<li>
+										10 Multi subject wise tests
+									</li>
+									<li>06 Full length tests</li>
+
+								</ul>
+							</div>
+						</div>
+						<h3 class="text-center text-blue">Practice 800+ Questions, Take quizzes and many more...</h3>
+					</div>
+
+					
+					<div id="tab2" class="c-tabs">
+
+					  <div class="flex-row">
+							<div class="flex-col col-6">
+								<h2 class="blue-heading text-center">
+									GATE CE 2021 Test series <br> 140+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										76 Topic wise tests
+									</li>
+									<li>04 Multi subject wise tests
+									</li>
+									<li>
+										24 Subject wise tests
+
+									</li>
+									
+									<li>13 Full length tests</li>
+									<li>12 Previous year full paper tests</li>
+									<li>15 Previous year subject wise tests</li>
+
+								</ul>
+							</div>
+							<div class="flex-col col-6 rtbm" style="border-left: 1px solid var(--theme-color);
+						">
+								<h2 class="blue-heading text-center">
+									IES CE 2021 Test Series<br> 60+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										40 Topic wise tests
+									</li>
+									
+									<li>
+										10 Subject wise tests
+
+									</li>
+									<li>
+										10 Multi subject wise tests
+									</li>
+									<li>6 Full length tests</li>
+
+								</ul>
+							</div>
+						</div>
+						<h3 class="text-center text-blue">Practice 300+ Questions, Take quizzes and many more...</h3>
+					</div>
+
+					<div id="tab3" class="c-tabs">
+
+					  <div class="flex-row">
+							<div class="flex-col col-6">
+								<h2 class="blue-heading text-center">
+									GATE EE 2021 Test series<br> 140+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										82 Topic wise tests
+									</li>
+									<li>05 Multi subject wise tests
+									</li>
+									<li>
+										24 Subject wise tests
+
+									</li>
+									
+									<li>12 Full length tests</li>
+									<li>9 Previous year full paper tests</li>
+									<li>20 Previous year subject wise tests</li>
+
+								</ul>
+							</div>
+							<div class="flex-col col-6 rtbm" style="border-left: 1px solid var(--theme-color);
+						">
+								<h2 class="blue-heading text-center">
+									IES EE 2021 Test Series <br> 60+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										40 Topic wise tests
+									</li>
+									
+									<li>
+										10 Subject wise tests
+
+									</li>
+									<li>
+										10 Multi subject wise tests
+									</li>
+									<li>6 Full length tests</li>
+
+								</ul>
+							</div>
+						</div>
+						<h3 class="text-center text-blue">Practice 1300+ Questions, Take quizzes and many more...</h3>
+					</div>					
+					
+					<div id="tab4" class="c-tabs">
+
+					  <div class="flex-row">
+							<div class="flex-col col-6">
+								<h2 class="blue-heading text-center">
+									GATE EC 2021 Test series<br> 140+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										67 Topic wise tests
+									</li>
+									<li>4 Multi subject wise tests
+									</li>
+									<li>
+										20 Subject wise tests
+
+									</li>
+									
+									<li>12 Full length tests</li>
+									<li>11 Previous year full paper tests</li>
+									<li>10 Previous year subject wise tests</li>
+
+								</ul>
+							</div>
+							<div class="flex-col col-6 rtbm" style="border-left: 1px solid var(--theme-color);
+						">
+								<h2 class="blue-heading text-center">
+									IES EC 2021 Test Series<br> 60+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										40 Topic wise tests
+									</li>
+									
+									<li>
+										10 Subject wise tests
+
+									</li>
+									<li>
+										10 Multi subject wise tests
+									</li>
+									<li>6 Full length tests</li>
+
+								</ul>
+							</div>
+						</div>
+						<h3 class="text-center text-blue">Practice 1300+ Questions, Take quizzes and many more...</h3>
+					</div>
+
+					<div id="tab5" class="c-tabs">
+
+					  <div class="flex-row">
+							<div class="flex-col col-6">
+								<h2 class="blue-heading text-center">
+									GATE IN 2021 Test series<br> 30+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									
+									<li>
+										20 Subject wise tests
+									</li>
+									
+									<li>10 Full length tests</li>
+									<li>5 Previous year full paper tests</li>
+									
+								</ul>
+							</div>
+							<div class="flex-col col-6 rtbm" style="border-left: 1px solid var(--theme-color);
+						">
+								<h2 class="blue-heading text-center">
+									IES IN 2021 Test Series<br> 60+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										40 Topic wise tests
+									</li>
+									
+									<li>
+										10 Subject wise tests
+
+									</li>
+									<li>
+										10 Multi subject wise tests
+									</li>
+									<li>6 Full length tests</li>
+
+								</ul>
+							</div>
+						</div>
+						<h3 class="text-center text-blue"></h3>
+					</div>
+
+					<div id="tab6" class="c-tabs">
+
+					  <div class="text-center">
+								<h2 class="blue-heading text-center">
+									GATE CS 2021 Test series<br> 140+ Total Tests
+
+								</h2>
+								<br>
+								<ul class="simple-list semibold">
+									<li>
+										64 Topic wise tests
+									</li>
+									<li>05 Multi subject wise tests
+									</li>
+									<li>
+										22 Subject wise tests
+
+									</li>
+									
+									<li>12 Full length tests</li>
+									<li>11 Previous year full paper tests</li>
+									<li>10 Previous year subject wise tests</li>
+
+								</ul>
+														
+						</div>
+						<h3 class="text-center text-blue"></h3>
+					</div>
+
+					<div id="tab7" class="c-tabs">
+
+					  <div class="">
+							<div class="text-center">
+								<h2 class="blue-heading text-center">
+									GATE CH 2021 Test series<br> 100+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									
+									<li>
+										73 Subject wise tests
+
+									</li>
+									
+									<li>03 Full length tests</li>
+									<li>11 Previous year full paper tests</li>
+									<li>16 Previous year subject wise tests</li>
+
+								</ul>
+							</div>
+						
+						</div>
+						<h3 class="text-center text-blue"></h3>
+					</div>
+
+					<div id="tab8" class="c-tabs">
+
+					  <div class="">
+							<div class="text-center">
+								<h2 class="blue-heading text-center">
+									GATE BT 2021 Test series<br> 100+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									
+									<li>
+										75 Subject wise tests
+
+									</li>
+									
+									<li>08 Full length tests</li>
+									<li>08 Previous year full paper tests</li>
+									<li>09 Previous year subject wise tests</li>
+
+								</ul>
+							</div>
+							
+						</div>
+						<h3 class="text-center text-blue"></h3>
+					</div>
+
+					<div id="tab9" class="c-tabs">
+
+					  <div class="">
+							<div class="text-center">
+								<h2 class="blue-heading text-center">
+									GATE AE 2021 Test series<br> 50+ Total Tests
+
+								</h2>
+								<ul class="simple-list semibold">
+									<li>
+										40 Subject wise tests
+
+									</li>
+									
+									<li>05 Full length tests</li>
+									<li>10 Previous year full paper tests</li>
+									
+								</ul>
+							</div>
+						
+						</div>
+						<h3 class="text-center text-blue"></h3>
+					</div>
+
+
+				  </section>
+				  </div>
+			</div>
+		</div>
