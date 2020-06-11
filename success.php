7df582c543023ab9476c8f478405296d821fdbed
Sandeep <sandeep@edunuts.com>
2020-06-11 10:34:13 +0530

    forms process

---
+++ success.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link rel="stylesheet" href="css/splitting.css">
+	<link rel="stylesheet" href="css/base.css">
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	
+	<title>IES GATE ACADEMY</title>
+</head>
+  <?php include 'inc/header.php' ?>
+
+	<div class="c-container spacer">
+		<h2 class="title  text-center text-theme no-margin">
+			Your Enquiry Successfully submitted .
+		</h2>
+		<p class="text-center">Thank you for your enquiry. we will get back to you.</p>
+           <p class="text-center">Regards IES Academy</p>
+		</p>
+		<br>
+		<div class="text-center">
+		 <a class="btn-theme " href="index.php">Go To Home</a>
+		</div>
+		<br>
+
+		
+	</div>
+
+	<footer class="site-footer">
+		<div class="c-container">
+			<div class="flex-row" style="flex-wrap: nowrap;">
+				<div class="flex-col">
+					<ul class="simple-list">
+						<h4>Contact Info</h4>
+						<li>
+							<a href="">
+								D.No. 50, Mahalakshmi Street, <br>
+								Beside City Union Bank, T.Nagar, <br>
+								Chennai-600017. <br>
+							</a>
+						</li>
+						<li>
+							<a href="">+91-9445017000</a>
+						</li>
+						<li>
+							<a href="">+91-9445027000</a>
+						</li>
+						<li>
+							<a href="">+91-9445037000</a>
+						</li>
+						<li>
+							<a href="">iesgateacademy@gmail.com</a>
+						</li>
+						
+	
+	
+					</ul>
+				</div>
+				
+				<div class="flex-col">
+					<ul class="simple-list">
+						<h4>Quick Links</h4>
+						<li><a href="">ESE</a></li>
+						<li><a href="">GATE</a></li>
+						<li><a href="">PSUs</a></li>
+						<li><a href="">Courses</a></li>
+						<li><a href="">- Classroom Courses</a></li>
+						<li><a href="">- Online Live Classes</a></li>
+						<li><a href="">- Online Test Series  </a></li>
+						<li><a href="">Batches & Fee Structure</a></li>
+						<li><a href="">Contact us</a></li>
+						<li><a href="">Enquiry</a></li>
+						
+					</ul>
+				</div>
+				<div class="flex-col social-col">
+					<ul class="simple-list">
+						<h4>Social</h4>
+						<div class="flex-inline-mobile">
+							<li>
+								<a href="">
+								<img src="img/icons/facebbok.png" alt="">					
+								</a>
+							</li>
+							<li><a href=""><img src="img/icons/insta.png" alt=""></a></li>
+							<li><a href=""><img src="img/icons/whatsapp.png" alt=""></a></li>
+							<li><a href=""><img src="img/icons/twitter.png" alt=""></a></li>
+							<li><a href=""><img src="img/icons/gp.png" alt=""></a></li>
+							<li><a href=""><img src="img/icons/youtube.png" alt=""></a></li>
+						</div>
+						
+					
+					
+	
+					</ul>
+				</div>
+				<div class="flex-col sbs-col">
+					<div class="subscribe">
+						<h4 class="text-theme">Mailing List</h4>
+						<p class="text-white">
+							Enter your email address  for our mailing list to keep yourself updated.
+						</p>
+						<div class="attach-input">
+							<input type="text" placeholder="Email address">
+							<button >Done</button>
+						</div>
+						<p style="color: #919191;">We Respect Your Privacy</p>
+						
+					</div>
+					<p class="copywrite">IES GATE ACADEMY © 2020. All Rights Reserved.</p>
+				</div>
+			</div>
+		</div>
+	
+	</footer>
+
+<script src="js/jQuery.min.js"></script>
+<script src="js/scroll-out.js"></script>
+<script src="js/swiper.min.js"></script>
+<script src="js/splitting.js"></script>
+<script src="js/micromodal.min.js"></script>
+<script src="js/jquery.marquee.js"></script>
+<script src="js/controller.js"></script>
+	<script>
+
+
+	</script>
+</body>
+</html>
