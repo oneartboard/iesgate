e272aa2c55851ac8a17f6617145eead5bfab9884
Sandeep <sandeep@edunuts.com>
2020-06-11 11:33:24 +0530

    forms process

--- send-email.php
+++ send-email.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	 function sendEmail($data,$type)
 	 {
 	     $subject = "";
@@ -7,7 +7,6 @@
 	       }else{
 	       	 $subject = 'New Enroll From '.$data['name'];
 	       }
-
 	       $to      = 'pranaypydi@gmail.com';
 	       $from      = $data['email'];
 	       $message = '<p>Name: <b>&nbsp'.$data['name'].'</b></p>
@@ -20,7 +19,6 @@
 	 				<p>Selected Stream: <b>&nbsp;'. $data['stream'] .'</b></p>
 	 				<p>Message: <b>&nbsp;'. $data['message'] ?? " " .'</b></p>
 	 				';
-	  
 	 			  if ($data && $subject && $type && $to) {
 	 			  	 $headers = 'From: '.$from.'' . "\r\n" .
 	 			          'Reply-To: '.$from.'' . "\r\n" .
@@ -29,21 +27,14 @@
 	 			       echo true;
 	 			  }
 	 }
-
-
 	  function sendSMS($phone)
 	  {
 	  	$url = 'http://sms.adeep.in/api/v4/?api_key=A1b509a4a9cd046eb888085ad8192387d&method=sms&sender=IGADMY&to='.$phone.'&message=Thank you for your enquiry. we will get back to you .Regards IES Academy';
 	 	$ch = curl_init($url);
-
 	 	curl_setopt($ch, CURLOPT_POST, 1);
 	 	curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
 	 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	 	$response = curl_exec($ch);
 	 	curl_close($ch);
 	  }
-      
-     
-    
-
  ?>
