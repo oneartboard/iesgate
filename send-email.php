fbd7d412b3d71c6d52b4346d732644ee9e074479
Sandeep <sandeep@edunuts.com>
2020-06-11 11:36:39 +0530

    forms process

--- send-email.php
+++ send-email.php
@@ -23,7 +23,7 @@
 	 			  	 $headers = 'From: '.$from.'' . "\r\n" .
 	 			          'Reply-To: '.$from.'' . "\r\n" .
 	 			          'X-Mailer: PHP/' . phpversion();
-	 			       mail($to, $subject, $message, $headers);
+	 			       //mail($to, $subject, $message, $headers);
 	 			       echo true;
 	 			  }
 	 }
